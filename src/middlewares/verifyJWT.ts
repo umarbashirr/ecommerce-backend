186ec5e8fa48e2b0b8adafb4b0cdec2b16a02896
umarbashirr <umarbashir93@outlook.com>
2024-02-17 19:32:13 +0530

    Delete unused middlewares

---
+++ src/middlewares/verifyJWT.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import ApiResponse from "../utils/apiResponse";
+
+export const verifyJWT = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json(new ApiResponse(401, "Unauthorized"));
+  }
+
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET as string);
+
+  if (!decoded) {
+    return res.status(401).json(new ApiResponse(401, "Unauthorized"));
+  }
+
+  req.user = decoded;
+
+  next();
+};
