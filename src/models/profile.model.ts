c123df9ac7c4fc922063b9e03c75c23bc1e257af
umarbashirr <umarbashir93@outlook.com>
2024-02-17 22:27:51 +0530

    Add address and social schemas to profile model

--- src/models/profile.model.ts
+++ src/models/profile.model.ts
@@ -1,5 +1,78 @@
 import { Schema, model } from "mongoose";
 
+const addressSchema = new Schema({
+  street: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  state: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const socialSchema = new Schema({
+  facebook: {
+    type: String,
+    trim: true,
+  },
+  twitter: {
+    type: String,
+    trim: true,
+  },
+  instagram: {
+    type: String,
+    trim: true,
+  },
+});
+
+interface IProfileSchema {
+  firstName: string;
+  lastName: string;
+  bio: string;
+  phoneNumber: string;
+  billingAddress: {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+  };
+  shippingAddress: {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+  };
+  social: {
+    facebook: string;
+    twitter: string;
+    instagram: string;
+  };
+  user: any;
+  wishlist: string[];
+  cart: string[];
+  orders: string[];
+}
+
 const profileSchema = new Schema(
   {
     firstName: {
@@ -21,16 +94,13 @@ const profileSchema = new Schema(
       trim: true,
     },
     billingAddress: {
-      type: Schema.Types.ObjectId,
-      ref: "Address",
+      type: addressSchema,
     },
     shippingAddress: {
-      type: Schema.Types.ObjectId,
-      ref: "Address",
+      type: addressSchema,
     },
     social: {
-      type: Schema.Types.ObjectId,
-      ref: "Social",
+      type: socialSchema,
     },
     user: {
       type: Schema.Types.ObjectId,
@@ -60,6 +130,6 @@ const profileSchema = new Schema(
   }
 );
 
-const Profile = model("Profile", profileSchema);
+const Profile = model<IProfileSchema>("Profile", profileSchema);
 
 export default Profile;
