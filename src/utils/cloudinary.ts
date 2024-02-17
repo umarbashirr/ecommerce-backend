afe490d7916077c18f4844df89eebf07d1b52685
umarbashirr <umarbashir93@outlook.com>
2024-02-17 21:17:14 +0530

    Add Cloudinary utility for uploading files

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (filePath: string) => {
+  try {
+    if (!filePath) return null;
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    fs.unlinkSync(filePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(filePath);
+    return null;
+  }
+};
+
+export default uploadToCloudinary;
