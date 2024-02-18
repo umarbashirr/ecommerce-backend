668db08ac615d26066000d0ca77d8a78c94c8f96
umarbashirr <umarbashir93@outlook.com>
2024-02-18 18:34:21 +0530

    Add JWT verification middleware to wishlist routes

--- src/routes/wishlist.routes.ts
+++ src/routes/wishlist.routes.ts
@@ -4,8 +4,11 @@ import {
   updateWishlistCtrl,
   deleteWishlistCtrl,
 } from "../controllers/wishlist.controller";
+import { verifyJWT } from "../middlewares/verifyToken.middleware";
 
 const router = Router();
+
+router.use(verifyJWT);
 
 router.route("/").get(getWishlistCtrl).put(updateWishlistCtrl);
 router.route("/:id").delete(deleteWishlistCtrl);
